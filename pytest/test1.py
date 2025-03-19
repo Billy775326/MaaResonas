b7fc97389a149a245f1c48c45406a7057e6f3fce
Billy775326 <billy775326@gmail.com>
2025-03-19 19:01:41 +0800

    clear dir

--- pytest/test1.py
+++
@@ -1,8 +0,0 @@
-import requests
-from lxml import etree
-url="https://www.resonance-columba.com/route"
-html_str = requests.get(url)
-html_str.encoding = "utf-8"
-xpath_str = etree.HTML(html_str.text)
-data_price = xpath_str.xpath('//table')
-print(data_price)
